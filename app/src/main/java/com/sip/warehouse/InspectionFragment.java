4fee8ee2a09171d472b6e9f48f41aa09f17064f3
Denny Ramdhani Zain <duniaku2.ramdhani@gmail.com>
2019-02-23 22:29:06 +0700

    add step layout for inspection menu

--- app/src/main/java/com/sip/warehouse/InspectionFragment.java
+++ app/src/main/java/com/sip/warehouse/InspectionFragment.java
@@ -9,16 +9,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
 
 
+public class InspectionFragment extends Fragment implements StepperLayout.StepperListener{
 
-public class InspectionFragment extends Fragment {
+
+    private StepperLayout mStepperLayout;
+    private StepperAdapterInspection mStepperAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inspection, container, false);
+
+        mStepperLayout = (StepperLayout) view.findViewById(R.id.stepperLayout2);
+
+        mStepperAdapter = new StepperAdapterInspection(getFragmentManager(), getActivity());
+        mStepperLayout.setAdapter(mStepperAdapter);
+        mStepperLayout.setListener(this);
         return view;
     }
+
+    @Override
+    public void onCompleted(View completeButton) {
+        Toast.makeText(getActivity(), "onCompleted!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(VerificationError verificationError) {
+        Toast.makeText(getActivity(), "onError! -> " + verificationError.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStepSelected(int newStepPosition) {
+
+    }
+
+    @Override
+    public void onReturn() {
+        getActivity().finish();
+    }
+
 
 }
---
+++ app/src/main/java/com/sip/warehouse/InspectionOne.java
@@ -0,0 +1,76 @@
+package com.sip.warehouse;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stepstone.stepper.BlockingStep;
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InspectionOne extends Fragment implements BlockingStep {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inspection_one, container, false);
+    }
+
+    @Override
+    public void onNextClicked(final StepperLayout.OnNextClickedCallback callback) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //you can do anythings you want
+                callback.goToNextStep();
+
+            }
+        }, 0L);// delay open another fragment,
+
+    }
+
+    @Override
+    public void onCompleteClicked(StepperLayout.OnCompleteClickedCallback callback) {
+
+    }
+
+    @Override
+    public void onBackClicked(final StepperLayout.OnBackClickedCallback callback) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //you can do anythings you want
+                callback.goToPrevStep();
+
+            }
+        }, 0L);// delay open another fragment,
+    }
+
+    @Override
+    public VerificationError verifyStep() {
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
+    }
+
+}
