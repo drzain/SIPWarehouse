49c693306ac46f4da4d0bf19de4b75e1ec626b33
Denny Ramdhani Zain <duniaku2.ramdhani@gmail.com>
2019-03-07 21:00:41 +0700

    add listview for question

--- app/src/main/java/com/sip/warehouse/AppConfig.java
+++ app/src/main/java/com/sip/warehouse/AppConfig.java
@@ -8,4 +8,6 @@ public class AppConfig {
     // Server user logout url
     public static String URL_LOGOUT = "https://sip.uridu.id/api/v1/auth/logout";
 
+    public static String URL_QUESTION_RECEIVE = "http://localhost:8000/api/questions";
+
 }
